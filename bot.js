ac1fca395b41079319bf85a3e0cbe0e9e58058fc
Ahmadalalfe <46822450+Ahmadalalfe@users.noreply.github.com>
2019-02-05 09:31:17 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -177,6 +177,47 @@ client.on("message", message => { //clear
           footer: {
             
           }
+
+
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("541660472710135811");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("541660472710135823");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("541660472710135811");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
         }}).then(msg => {msg.delete(3000)});
                             }
   
